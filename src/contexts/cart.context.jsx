8256d473b7154339219c92549274b71cb8c9538c
Nikhil <nikhil@mac.local>
2023-06-11 08:08:15 +0530

    Mirated to Reducers in Contexts

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,16 +1,154 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useReducer } from "react";
+import { createAction } from "../utils/reducer.utils";
 
 export const CartContext = createContext({
-  isCartOpen: false,
-  setIsCartOpen: () => {},
-  cartItems: [],
-  addItemToCart: () => {},
-  removeItemFromCart: () => {},
-  clearItemFromCart: () => {},
-  cartCount: 0,
-  cartTotal: 0,
+	isCartOpen: false,
+	setIsCartOpen: () => {},
+	cartItems: [],
+	addItemToCart: () => {},
+	removeItemFromCart: () => {},
+	clearItemFromCart: () => {},
+	cartCount: 0,
+	cartTotal: 0,
 });
 
+// ADD CART ITEM
+const addCartItem = (cartItems, productToAdd) => {
+	const existingCartItem = cartItems.find(
+		(cartItem) => cartItem.id === productToAdd.id
+	);
+
+	if (existingCartItem) {
+		return cartItems.map((cartItem) =>
+			cartItem.id === productToAdd.id
+				? { ...cartItem, quantity: cartItem.quantity + 1 }
+				: cartItem
+		);
+	}
+
+	return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
+
+//REDUCE CART ITEM QUANTITY
+const removeCartItem = (cartItems, cartItemToRemove) => {
+	const existingCartItem = cartItems.find(
+		(cartItem) => cartItem.id === cartItemToRemove.id
+	);
+	if (existingCartItem.quantity === 1) {
+		return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+	}
+	return cartItems.map((cartItem) =>
+		cartItem.id === cartItemToRemove.id
+			? { ...cartItem, quantity: cartItem.quantity - 1 }
+			: cartItem
+	);
+};
+
+//REMOVE A CART ITEM
+const clearCartItem = (cartItems, cartItemToClear) =>
+	cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id);
+
+//
+//? INITIAL STATE OBJECT
+const INITIAL_STATE = {
+	isCartOpen: false,
+	cartItems: [],
+	cartCount: 0,
+	cartTotal: 0,
+};
+
+const CART_ACTION_TYPES = {
+	SET_CART_ITEMS: "SET_CART_ITEMS",
+	SET_IS_CART_OPEN: "SET_IS_CART_OPEN",
+};
+
+//? CART-REDUCER
+const cartReducer = (state, action) => {
+	const { type, payload } = action;
+	switch (type) {
+		case CART_ACTION_TYPES.SET_CART_ITEMS:
+			return {
+				...state,
+				...payload,
+			};
+		case CART_ACTION_TYPES.SET_IS_CART_OPEN:
+			return {
+				...state,
+				isCartOpen: payload,
+			};
+		default:
+			throw new Error(`Unhandled Type of ${type} in cartReducer`);
+	}
+};
+
+export const CartProvider = ({ children }) => {
+	const [state, dispatch] = useReducer(cartReducer, INITIAL_STATE);
+	const { cartCount, cartItems, cartTotal, isCartOpen } = state;
+
+	const updateCartItemsReducer = (newCartItems) => {
+		const newCartTotal = newCartItems.reduce(
+			(currentTotal, cartItem) =>
+				currentTotal + cartItem.quantity * cartItem.price,
+			0
+		);
+		const newCartCount = newCartItems.reduce(
+			(currentTotal, cartItem) => currentTotal + cartItem.quantity,
+			0
+		);
+		// dispatch({
+		// 	type: CART_ACTION_TYPES.SET_CART_ITEMS,
+		// 	payload: {
+		// 		cartItems: newCartItems,
+		// 		cartTotal: newCartTotal,
+		// 		cartCount: newCartCount,
+		// 	},
+		// });
+		dispatch(
+			createAction(CART_ACTION_TYPES.SET_CART_ITEMS, {
+				cartItems: newCartItems,
+				cartTotal: newCartTotal,
+				cartCount: newCartCount,
+			})
+		);
+	};
+
+	const setIsCartOpen = (isCartOpen) => {
+		// dispatch({ type: CART_ACTION_TYPES.SET_IS_CART_OPEN, payload: isCartOpen });
+		dispatch(createAction(CART_ACTION_TYPES.SET_IS_CART_OPEN, isCartOpen));
+	};
+
+	const addItemToCart = (productToAdd) => {
+		// setCartItems(addCartItem(cartItems, productToAdd));
+		const newCartItems = addCartItem(cartItems, productToAdd);
+		updateCartItemsReducer(newCartItems);
+	};
+
+	const removeItemFromCart = (cartItemToRemove) => {
+		// setCartItems(removeCartItem(cartItems, cartItemToRemove));
+		const newCartItems = removeCartItem(cartItems, cartItemToRemove);
+		updateCartItemsReducer(newCartItems);
+	};
+	const clearItemFromCart = (cartItemToClear) => {
+		// setCartItems(clearCartItem(cartItems, cartItemToClear));
+		const newCartItems = clearCartItem(cartItems, cartItemToClear);
+		updateCartItemsReducer(newCartItems);
+	};
+
+	const value = {
+		isCartOpen,
+		setIsCartOpen,
+		addItemToCart,
+		cartItems,
+		cartCount,
+		removeItemFromCart,
+		clearItemFromCart,
+		cartTotal,
+	};
+
+	return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
+};
+
+/*
 const addCartItem = (cartItems, productToAdd) => {
   const existingCartItem = cartItems.find(
     (cartItem) => cartItem.id === productToAdd.id
@@ -95,3 +233,4 @@ export const CartProvider = ({ children }) => {
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
+*/
--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,30 +1,63 @@
-import { createContext, useEffect, useState } from "react";
-import { createUserDocumentFromAuth, onAuthStateChangedListener } from "../utils/firebase.utils";
-
+import { createContext, useEffect, useReducer, useState } from "react";
+import {
+	createUserDocumentFromAuth,
+	onAuthStateChangedListener,
+} from "../utils/firebase.utils";
 
 export const UserContext = createContext({
-    currentUser: null,
-    setCurrentUser: () => null,
+	currentUser: null,
+	setCurrentUser: () => null,
 });
 
+const USER_ACTION_TYPES = {
+	SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+const userReducer = (state, action) => {
+	const { type, payload } = action;
+
+	switch (type) {
+		case USER_ACTION_TYPES.SET_CURRENT_USER:
+			return {
+				...state,
+				currentUser: payload,
+			};
+		default:
+			throw new Error(`Unhandled type ${type} in userReducer`);
+	}
+};
+const INITIAL_STATE = {
+	currentUser: null,
+};
+
 export const UserProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(null);
-    // const value = {currentUser: currentUser,setCurrentUser:setCurrentUser};
-    const value = { currentUser, setCurrentUser };
+	//  const [currentUser, setCurrentUser] = useState(null);
+	//  We are using useReducer now =>
 
-    //other way to see loggedin or logged out
-    useEffect(() => {
-        const unsubscribe = onAuthStateChangedListener(async (user) => {
-            if (user) {
-                const docref = await createUserDocumentFromAuth(user);
-                console.log('doc ref', docref);
-            }
-            setCurrentUser(user);
-            console.log(user);
-        })
-        return unsubscribe
-        //return calls when unmount -> unsubscribe when unmount - stops onAuthStateChangeListener listening
-    }, [])
+	// const [{currentUser},dispatch]  = useReducer(userReducer,INITIAL_STATE)
+	const [state, dispatch] = useReducer(userReducer, INITIAL_STATE);
+	const { currentUser } = state;
 
-    return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
-}
+	const setCurrentUser = (user) => {
+		dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+	};
+
+	// const value = {currentUser: currentUser,setCurrentUser:setCurrentUser};
+	const value = { currentUser, setCurrentUser };
+
+	//other way to see logged-in or logged out
+	useEffect(() => {
+		const unsubscribe = onAuthStateChangedListener(async (user) => {
+			if (user) {
+				await createUserDocumentFromAuth(user);
+				// console.log("doc ref", docref);
+			}
+			setCurrentUser(user);
+			// console.log(user);
+		});
+		return unsubscribe;
+		//return calls when unmount -> unsubscribe when unmount - stops onAuthStateChangeListener listening
+	}, []);
+
+	return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+};
